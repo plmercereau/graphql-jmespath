24f30d83560e9dc14ad022a95a236bcfae8ae278
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-04-18 15:44:50 +0200

    chore: add official examples

--- src/tests/todo.ts
+++ src/tests/todo.ts
@@ -6,6 +6,18 @@ functions
 pipe
 wildcard
 current @
+
+
+https://jmespath.org/examples.html
+
+people[?age > `20`].[name, age]
+people[?age > `20`].{name: name, age: age}
+people[*].{name: name, tags: tags[0]}
+people[?general.id==`100`].general | [0]
+people[?general.id==`100`] | [0].general
+sort_by(Contents, &Date)[*].{Key: Key, Size: Size}
+locations[?state == 'WA'].name | sort(@)[-2:] | {WashingtonCities: join(', ', @)}
+
 */
 
 export const todo: Expression[] = []
